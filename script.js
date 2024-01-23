5d586ea5bd9a1824651f26f38a6fb1049400ca1b
Isuru Eranda <132377805+Isuru-Eranda@users.noreply.github.com>
2024-01-23 23:52:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,19 +25,3 @@ function closemenu() {
 
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx25Rcs4AAA9ZM-SYWPrR2bfJT8KFE9RQ5zjzo9sqck-_yOLmct1WOnLoQscIR6K8nTQg/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById('msg')
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-})
