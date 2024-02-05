3faa77fd16e3b9a1e387d2588a2e67f5a75600ca
Isuru Eranda <132377805+Isuru-Eranda@users.noreply.github.com>
2024-02-06 00:18:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbx25Rcs4AAA9ZM-SYWPrR
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById('msg')
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
